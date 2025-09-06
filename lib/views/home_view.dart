bcffea47c4682db07da353ef8cc316fc502fe4c8
Mohamed Elerian <103853528+Elerian07@users.noreply.github.com>
2025-09-06 14:08:19 +0300

    Initialize Flutter project structure
    Add initial files for a new Flutter app, including platform-specific directories and configuration for Android, iOS, Linux, macOS, Windows, and web. Includes project metadata, build scripts, asset files, and basic Dart source code.

---
+++ lib/views/home_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("News"),
+            Text("Cloud", style: TextStyle(color: Colors.orange)),
+          ],
+        ),
+      ),
+    );
+  }
+}
